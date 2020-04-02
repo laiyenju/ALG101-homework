7610e01e27f3f214f3f454797370e925b6f8b714
Lai Yen Ju <yenjulai.1991@gmail.com>
2020-04-02 15:44:13 +0800

    Update project2-1-second-minimal.py

--- project2-1-second-minimal.py
+++ project2-1-second-minimal.py
@@ -2,7 +2,7 @@ list = [0, 0, 45, 6, 7, 100, -1]
 min1 = list[0]
 min2 = list[0]
 
-for i in range(len(list)):
+for i in range(len(list)):  # 這裡以 len() 設定 list 長度範圍，避免直接使用 list 出現的 out of index 問題
     if list[i] < min1:
         min2 = min1
         min1 = list[i]
